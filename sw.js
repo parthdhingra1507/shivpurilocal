ae1373c5bd626ef0c1bfc167d991084dd4957168
parthdhingra1507 <parthdhingra15@gmail.com>
2025-12-06 09:50:50 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -9,9 +9,31 @@ const ASSETS = [
 
 // Install Event
 self.addEventListener('install', (event) => {
+    console.log('Service Worker installing...');
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then((cache) => cache.addAll(ASSETS))
+            .then((cache) => {
+                console.log('Caching assets');
+                return cache.addAll(ASSETS);
+            })
+            .then(() => self.skipWaiting())
+    );
+});
+
+// Activate Event
+self.addEventListener('activate', (event) => {
+    console.log('Service Worker activating...');
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        console.log('Deleting old cache:', cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        }).then(() => self.clients.claim())
     );
 });
 
@@ -19,6 +41,25 @@ self.addEventListener('install', (event) => {
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request)
-            .then((response) => response || fetch(event.request))
+            .then((response) => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request).then((response) => {
+                    // Don't cache non-successful responses
+                    if (!response || response.status !== 200 || response.type !== 'basic') {
+                        return response;
+                    }
+                    const responseToCache = response.clone();
+                    caches.open(CACHE_NAME).then((cache) => {
+                        cache.put(event.request, responseToCache);
+                    });
+                    return response;
+                });
+            })
+            .catch(() => {
+                // Return offline page if available
+                return caches.match('/');
+            })
     );
 });
